0eedb9144a841a1a731052c568ac793108ffd88a
AnilChinchawale <anil24593@gmail.com>
2023-06-05 12:40:20 +0530

    Apothem Network Test Added

--- tests/e2e/specs/network/apothem.spec.js
+++ tests/e2e/specs/network/apothem.spec.js
@@ -1,6 +1,6 @@
 import { ACCOUNT_1, ACCOUNT_2, ACCOUNT_3 } from "../../utils";
 
-const originAccount = "0x8d366250A96deBE81C8619459a503a0eEBE33ca6"
+const originAccount = "0x09C4183aC99B432c4BA78d354F8d21257bd15aaa"
 
 before(() => {
   cy.addMetamaskNetwork({
@@ -24,38 +24,38 @@ describe("Reading of XDC Apothem Network", () => {
     cy.get("[data-testid='asset-title-holder']").should("have.text", `Holder:${originAccount}`);
   });
 
-  // it("should be able to view surrendered escrow information", () => {
-  //   cy.visit("/verify");
-  //   cy.get("input[type=file]").attachFile("network/ebl-mumbai-surrendered.json");
-  //   cy.get("[id=surrendered-sign]").should("be.visible");
-  //   cy.get("[id=endorsement-chain-button]").click();
-  //   cy.wait(10000);
+  it("should be able to view surrendered escrow information", () => {
+    cy.visit("/verify");
+    cy.get("input[type=file]").attachFile("network/ebl-apothem-surrendered.json");
+    cy.get("[id=surrendered-sign]").should("be.visible");
+    cy.get("[id=endorsement-chain-button]").click();
+    cy.wait(50000);
 
-  //   cy.get("[data-testid='row-event-0'] [data-testid='action-title']").should("have.text", "Document has been issued");
-  //   cy.get("[data-testid='row-event-0'] [data-testid='row-event-Owner']").should("have.text", `Owner${originAccount}`);
-  //   cy.get("[data-testid='row-event-0'] [data-testid='row-event-Holder']").should("have.text", `Holder${originAccount}`);
+    cy.get("[data-testid='row-event-0'] [data-testid='action-title']").should("have.text", "Document has been issued");
+    cy.get("[data-testid='row-event-0'] [data-testid='row-event-Owner']").should("have.text", `Owner${originAccount}`);
+    cy.get("[data-testid='row-event-0'] [data-testid='row-event-Holder']").should("have.text", `Holder${originAccount}`);
 
-  //   cy.get("[data-testid='row-event-1'] [data-testid='action-title']").should("have.text", "Change Owners");
-  //   cy.get("[data-testid='row-event-1'] [data-testid='row-event-Owner']").should("have.text", `Owner${ACCOUNT_1}`);
-  //   cy.get("[data-testid='row-event-1'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_1}`);
+    cy.get("[data-testid='row-event-1'] [data-testid='action-title']").should("have.text", "Change Owners");
+    cy.get("[data-testid='row-event-1'] [data-testid='row-event-Owner']").should("have.text", `Owner${ACCOUNT_1}`);
+    cy.get("[data-testid='row-event-1'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_1}`);
 
-  //   cy.get("[data-testid='row-event-2'] [data-testid='action-title']").should("have.text", "Transfer holdership");
-  //   cy.get("[data-testid='row-event-2'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_2}`);
+    cy.get("[data-testid='row-event-2'] [data-testid='action-title']").should("have.text", "Transfer holdership");
+    cy.get("[data-testid='row-event-2'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_2}`);
 
-  //   cy.get("[data-testid='row-event-3'] [data-testid='action-title']").should("have.text", "Endorse change of ownership");
-  //   cy.get("[data-testid='row-event-3'] [data-testid='row-event-Owner']").should("have.text", `Owner${ACCOUNT_3}`);
+    cy.get("[data-testid='row-event-3'] [data-testid='action-title']").should("have.text", "Endorse change of ownership");
+    cy.get("[data-testid='row-event-3'] [data-testid='row-event-Owner']").should("have.text", `Owner${ACCOUNT_3}`);
 
-  //   cy.get("[data-testid='row-event-4'] [data-testid='action-title']").should("have.text", "Transfer holdership");
-  //   cy.get("[data-testid='row-event-4'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_3}`);
+    cy.get("[data-testid='row-event-4'] [data-testid='action-title']").should("have.text", "Transfer holdership");
+    cy.get("[data-testid='row-event-4'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_3}`);
 
-  //   cy.get("[data-testid='row-event-5'] [data-testid='action-title']").should("have.text", "Document surrendered to issuer");
+    cy.get("[data-testid='row-event-5'] [data-testid='action-title']").should("have.text", "Document surrendered to issuer");
     
-  //   cy.get("[data-testid='row-event-6'] [data-testid='action-title']").should("have.text", "Surrender of document rejected");
-  //   cy.get("[data-testid='row-event-6'] [data-testid='row-event-Owner']").should("have.text", `Owner${ACCOUNT_3}`);
-  //   cy.get("[data-testid='row-event-6'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_3}`);
+    cy.get("[data-testid='row-event-6'] [data-testid='action-title']").should("have.text", "Surrender of document rejected");
+    cy.get("[data-testid='row-event-6'] [data-testid='row-event-Owner']").should("have.text", `Owner${ACCOUNT_3}`);
+    cy.get("[data-testid='row-event-6'] [data-testid='row-event-Holder']").should("have.text", `Holder${ACCOUNT_3}`);
 
-  //   cy.get("[data-testid='row-event-7'] [data-testid='action-title']").should("have.text", "Document surrendered to issuer");
+    cy.get("[data-testid='row-event-7'] [data-testid='action-title']").should("have.text", "Document surrendered to issuer");
 
-  //   cy.get("[data-testid='row-event-8'] [data-testid='action-title']").should("have.text", "Surrender of document accepted");
-  // });
+    cy.get("[data-testid='row-event-8'] [data-testid='action-title']").should("have.text", "Surrender of document accepted");
+  });
 });
